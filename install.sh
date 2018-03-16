e876c6a36d69727875b27f9dcbeaf7227a425110
Jonathan Kinred <jkinred@linuxfoundation.org>
2018-03-16 15:37:02 +1100

    Be more specific about directories to link

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sets up links for dotfiles
-for file in .*; do echo "Symlinking $file" && ln -sf `pwd`/$file $HOME/; done
+for file in .[a-zA-Z]*; do echo "Symlinking $file" && ln -sf `pwd`/$file $HOME/; done
