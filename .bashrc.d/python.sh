771fe1356dc09eb1cde4806e790d3c90f318d394
Jonathan Kinred <jkinred@linuxfoundation.org>
2018-05-28 23:44:00 +0000

    Update pyenv

--- .bashrc.d/python.sh
+++ .bashrc.d/python.sh
@@ -1,7 +1,8 @@
 # Cache packages that pip downloads
 export PIP_DOWNLOAD_CACHE=~/.pip/cache
+
+# pyenv
 export PATH="~/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
 export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
-pyenv virtualenvwrapper
+eval "$(pyenv virtualenv-init -)"
